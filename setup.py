ab934029d22dd22b64242ec6c5a0c0968a6f744e
Rasmus Ansin <rasmus.ansin@gmail.com>
2021-01-31 14:55:50 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="conv",
+    version="1.0",
+    description="Converts between javascript/python object representations",
+    author="Rasmus Ansin",
+    author_email="rasmus.ansin+conv@gmail.com",
+    url="https://github.com/Ran4/conv",
+    install_requires=[
+    ],
+    packages=[
+        "conv",
+    ],
+)
